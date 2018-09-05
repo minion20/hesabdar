f85a8c17e8d2692777286e0cb0852d1c1c4a5cc0
mohammad <mohammadmashayekh20@gmail.com>
2018-09-05 08:54:21 -0700

    Income in models and som beatification

--- web/models.py
+++ web/models.py
@@ -7,6 +7,16 @@ from django.contrib.auth.models import User
 
 class Expense(models.Model):
     text=models.CharField(max_length=255)
-    date=models.DateTimeField('dakhele parantez')
+    date=models.DateTimeField('DAte')
     amount=models.BigIntegerField()
     user=models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date,self.amount)
+
+class Income (models.Model): # mige az models ye chizi daram ke Model hastesh
+    text=models.CharField(max_length=255)
+    date= models.DateTimeField('Date')
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date,self.amount)
